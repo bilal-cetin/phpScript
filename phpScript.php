0214d8c0146a0df504e7ff15d0f41bebcd6c7b69
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-05-17 16:02:20 +0300

    Post verileri database e yazıldı

--- phpScript.php
+++ phpScript.php
@@ -1,10 +1,21 @@
 <?php
 
+$connect = mysqli_connect("localhost","root","root","enforsec");
+
 $jsondata = file_get_contents("https://jsonplaceholder.typicode.com/posts");
  
 $array = json_decode($jsondata,true);
 
-print_r($array);
 
+foreach($array as $k =>$v){
+
+    $userId = mysqli_real_escape_string($connect, $v['userId']);
+    $id = mysqli_real_escape_string($connect, $v['id']);
+    $title = mysqli_real_escape_string($connect, $v['title']);
+    $body = mysqli_real_escape_string($connect, $v['body']);
+    $sql = "INSERT INTO posts(userId,id,title,body) VALUES ('".$userId."', '".$id."','".$title."', '".$body."')";
+    mysqli_query($connect,$sql);
+    
+}
 
 ?>
