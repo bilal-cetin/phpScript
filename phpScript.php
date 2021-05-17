a909efbce6841b416508eb849d872ca1ac4fffc0
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-05-17 16:11:32 +0300

    Comments verileri database e yazıldı

--- phpScript.php
+++ phpScript.php
@@ -4,10 +4,10 @@ $connect = mysqli_connect("localhost","root","root","enforsec");
 
 $jsondata = file_get_contents("https://jsonplaceholder.typicode.com/posts");
  
-$array = json_decode($jsondata,true);
+$postsArray = json_decode($jsondata,true);
 
 
-foreach($array as $k =>$v){
+foreach($postsArray as $k =>$v){
 
     $userId = mysqli_real_escape_string($connect, $v['userId']);
     $id = mysqli_real_escape_string($connect, $v['id']);
@@ -18,4 +18,22 @@ foreach($array as $k =>$v){
     
 }
 
+
+
+$jsondatas = file_get_contents("https://jsonplaceholder.typicode.com/comments");
+ 
+$commentsArray = json_decode($jsondatas,true);
+
+
+foreach($commentsArray as $k =>$v){
+
+    $postId = mysqli_real_escape_string($connect, $v['postId']);
+    $id = mysqli_real_escape_string($connect, $v['id']);
+    $name = mysqli_real_escape_string($connect, $v['name']);
+    $email = mysqli_real_escape_string($connect, $v['email']);
+    $body = mysqli_real_escape_string($connect, $v['body']);
+    $sql = "INSERT INTO comments(postId,id,name,email,body) VALUES ('".$postId."', '".$id."','".$name."', '".$email."', '".$body."')";
+    mysqli_query($connect,$sql);
+    
+}
 ?>
